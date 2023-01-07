752593c7e44eeb41a618cde6533b3655c8815f01
gye7072 <gye7072@bths.edu>
2023-01-06 23:17:43 -0500

    Finished binary, octal, and decimal conversions. Need to fix hexadecimal.

--- src/ConverterRunner.java
+++ src/ConverterRunner.java
@@ -5,22 +5,74 @@ class ConverterRunner {
     public static void main(String[] args) {
         System.out.println("Welcome to the Number Converter!");
         System.out.println("--------------------------------");
-        System.out.print("Enter the base of your number (2, 8 or 10): ");
+        System.out.print("Enter the base of your number (2, 8, 10, or 16): ");
 
         Scanner s = new Scanner(System.in);
         String choice = s.nextLine();
+        while((!choice.equals("2") && (!choice.equals("8")) && (!choice.equals("10")) && (!choice.equals("16")))){
+            System.out.println("This is an invalid response.Please try again.\n");
+            System.out.print("Enter the base of your number (2, 8, 10 or 16): ");
+            choice = s.nextLine();
+        }
         int base = Integer.parseInt(choice);
 
         System.out.print("Enter your number: ");
         String number = s.nextLine();
+
+        for (int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            if (base == 2) {
+                if (c != '0' && c != '1') {
+                    System.out.println("This is an invalid response. Please try again.");
+                    System.out.print("Enter your number: ");
+                    number = s.nextLine();
+                    i = -1;
+                }
+            } else if (base == 8) {
+                if (c < '0' || c > '7') {
+                    System.out.println("This is an invalid response. Please try again.");
+                    System.out.print("Enter your number: ");
+                    number = s.nextLine();
+                    i = -1;
+                }
+            } else if (base == 16) {
+                if (!((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F'))) {
+                    System.out.println("This is an invalid response. Please try again.");
+                    System.out.print("Enter your number: ");
+                    number = s.nextLine();
+                    i = -1;
+                }
+            }
+        }
+
         int n = Integer.parseInt(number);
 
         s.close();
 
         NumberConverter nc = new NumberConverter(n, base);
         int[] digits = nc.getDigits();
-        System.out.println("\n\nDigit array: " + Arrays.toString(nc.convertToBinary()));
-        System.out.println("Number: " + nc.displayOriginalNumber());
+
+        if(base == 2){
+            System.out.println("\nDecimal Number: " + Arrays.toString(nc.convertToDecimal()));
+            System.out.println("Octal Number: " + Arrays.toString(nc.convertToOctal()));
+            System.out.println("Hexadecimal Number: " + Arrays.toString(nc.convertToHexadecimal()));
+        }
+        if(base == 8){
+            System.out.println("\nDecimal Number: " + Arrays.toString(nc.convertToDecimal()));
+            System.out.println("Binary Number: " + Arrays.toString(nc.convertToBinary()));
+            System.out.println("Hexadecimal Number: " + Arrays.toString(nc.convertToHexadecimal()));
+        }
+        if(base == 10){
+            System.out.println("\nBinary Number: " + Arrays.toString(nc.convertToBinary()));
+            System.out.println("Octal Number: " + Arrays.toString(nc.convertToOctal()));
+            System.out.println("Hexadecimal Number: " + Arrays.toString(nc.convertToHexadecimal()));
+        }
+        if(base == 16){
+            System.out.println("\nDecimal Number: " + Arrays.toString(nc.convertToDecimal()));
+            System.out.println("Binary Number: " + Arrays.toString(nc.convertToBinary()));
+            System.out.println("Octal Number: " + Arrays.toString(nc.convertToOctal()));
+
+        }
     }
 }
 
--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -6,7 +6,7 @@ public class NumberConverter {
         String numberAsString = Integer.toString(number);
         digits = new int[numberAsString.length()];
         for (int i = 0; i < numberAsString.length(); i++) {
-            String single = numberAsString.substring(i,i+1);
+            String single = numberAsString.substring(i, i + 1);
             int d = Integer.parseInt(single);
             digits[i] = d;
         }
@@ -28,35 +28,170 @@ public class NumberConverter {
 
     public int[] convertToDecimal() {
         int[] base10 = new int[1];
-        if(base == 2){
-            for(int i = 0; i < digits.length; i++){
-                if(digits[i] == 1){
-                    base10[0] += Math.pow(2,digits.length-(1+i));
+        if (base == 2) {
+            for (int i = 0; i < digits.length; i++) {
+                if (digits[i] == 1) {
+                    base10[0] += Math.pow(2, digits.length - (1 + i));
                 }
             }
         }
-        if(base == 8){
-            for(int i = 0; i < digits.length; i++){
-                    base10[0] += digits[i] * Math.pow(8,digits.length-(1+i));
-                }
+        if (base == 8) {
+            for (int i = 0; i < digits.length; i++) {
+                base10[0] += digits[i] * Math.pow(8, digits.length - (1 + i));
             }
+        }
+        if (base == 16) {
+            for (int i = 0; i < digits.length; i++) {
+                int value = 0;
+                if (digits[i] >= 0 && digits[i] <= 9) {
+                    value = digits[i];
+                } else if (digits[i] >= 10 && digits[i] <= 15) {
+                    value = 10 + (digits[i] - 10);
+                }
+                base10[0] = (base10[0] * 16) + value;
+            }
+        }
         return base10;
     }
 
+
     public int[] convertToBinary() {
-        int[] base2 = new int[1];
-        int num = Integer.parseInt(displayOriginalNumber());
-        if(base == 10){
-            for(int i = 0; i < digits.length; i++){
-                base2[0] += num % 2;
-                num = num/2;
+        String base2 = "";
+        String originalNumberString = displayOriginalNumber().trim();
+        int num = Integer.parseInt(originalNumberString);
+        if (base == 10) {
+            while (num > 0) {
+                base2 = num % 2 + base2;
+                num = num / 2;
             }
         }
-        return base2;
+        if (base == 8) {
+            int base10 = 0;
+            for (int i = 0; i < digits.length; i++) {
+                base10 += digits[i] * Math.pow(8, digits.length - (1 + i));
+            }
+            while (base10 > 0) {
+                base2 = base10 % 2 + base2;
+                base10 = base10 / 2;
+            }
+
+        }
+        if(base == 16){
+            for (int i = 0; i < digits.length; i++) {
+                int value = 0;
+                if (digits[i] >= 0 && digits[i] <= 9) {
+                    value = digits[i];
+                } else if (digits[i] >= 10 && digits[i] <= 15) {
+                    value = 10 + (digits[i] - 10);
+                }
+                String valueBinary = Integer.toBinaryString(value);
+                while (valueBinary.length() < 4) {
+                    valueBinary = "0" + valueBinary;
+                }
+                base2 += valueBinary;
+            }
+        }
+
+        int[] arrayBase2 = new int[base2.length()];
+        for (int i = 0; i < base2.length(); i++) {
+            arrayBase2[i] = Character.getNumericValue(base2.charAt(i));
+        }
+        return arrayBase2;
     }
 
     public int[] convertToOctal() {
-        return null;
+        String base8 = "";
+        String originalNumberString = displayOriginalNumber().trim();
+        int num = Integer.parseInt(originalNumberString);
+        if (base == 10) {
+            while (num > 0) {
+                base8 = num % 8 + base8;
+                num = num / 8;
+            }
+        }
+        if (base == 2) {
+            int base10 = 0;
+            for (int i = 0; i < digits.length; i++) {
+                base10 += digits[i] * Math.pow(2, digits.length - (1 + i));
+            }
+            while (base10 > 0) {
+                base8 = base10 % 8 + base8;
+                base10 = base10 / 8;
+            }
+        }
+        if(base == 16){
+            int base10 = 0;
+            for (int i = 0; i < digits.length; i++) {
+                int value = 0;
+                if (digits[i] >= 0 && digits[i] <= 9) {
+                    value = digits[i];
+                } else if (digits[i] >= 10 && digits[i] <= 15) {
+                    value = 10 + (digits[i] - 10);
+                }
+                base10 += value * Math.pow(16, digits.length - (1 + i));
+            }
+            while (base10 > 0) {
+                base8 = base10 % 8 + base8;
+                base10 = base10 / 8;
+            }
+        }
+        int[] arrayBase8 = new int[base8.length()];
+        for (int i = 0; i < base8.length(); i++) {
+            arrayBase8[i] = Character.getNumericValue(base8.charAt(i));
+        }
+        return arrayBase8;
+    }
+
+
+    public int[] convertToHexadecimal() {
+        String hex = "";
+        String originalNumberString = displayOriginalNumber().trim();
+        int num = Integer.parseInt(originalNumberString);
+        if(base == 2){
+            num = 0;
+            for (int i = 0; i < digits.length; i++) {
+                if (digits[i] == 1) {
+                    num += Math.pow(2, digits.length - (1 + i));
+                }
+            }
+            base = 10;
+        }
+        if(base == 8){
+            num = 0;
+            for (int i = 0; i < digits.length; i++) {
+                num += digits[i] * Math.pow(8, digits.length - (1 + i));
+            }
+            base = 10;
+        }
+        if (base == 10) {
+            int n = num;
+            while (n > 0) {
+                int remainder = n % 16;
+                char c = ' ';
+                if (remainder >= 0 && remainder <= 9) {
+                    c = (char) ('0' + remainder);
+                } else if (remainder >= 10 && remainder <= 15) {
+                    c = (char) ('A' + (remainder - 10));
+                }
+                hex = c + hex;
+                n /= 16;
+            }
+
+            int[] hexInts = new int[hex.length()];
+            for (int i = 0; i < hex.length(); i++) {
+                char c = hex.charAt(i);
+                if (c >= '0' && c <= '9') {
+                    hexInts[i] = c - '0';
+                } else if (c >= 'A' && c <= 'F') {
+                    hexInts[i] = 10 + (c - 'A');
+                }
+            }
+        }
+        int[] arrayHex = new int[hex.length()];
+        for (int i = 0; i < hex.length(); i++) {
+            arrayHex[i] = Character.getNumericValue(hex.charAt(i));
+        }
+        return arrayHex;
     }
 }
 
