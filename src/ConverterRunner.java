870e5845951740fd6f6e83deec20a3849ae6c223
gye7072 <gye7072@bths.edu>
2023-01-09 19:29:29 -0500

    Need to fix the user input so it doesn't create an error

--- src/ConverterRunner.java
+++ src/ConverterRunner.java
@@ -5,74 +5,124 @@ class ConverterRunner {
     public static void main(String[] args) {
         System.out.println("Welcome to the Number Converter!");
         System.out.println("--------------------------------");
-        System.out.print("Enter the base of your number (2, 8, 10, or 16): ");
-
-        Scanner s = new Scanner(System.in);
-        String choice = s.nextLine();
-        while((!choice.equals("2") && (!choice.equals("8")) && (!choice.equals("10")) && (!choice.equals("16")))){
+        System.out.println("Do you want to do basic conversion or any conversion? (Only basic or any are viable inputs)");
+        Scanner s1 = new Scanner(System.in);
+        String input = s1.nextLine();
+        while (!input.equals("any") && !input.equals("basic")) {
             System.out.println("This is an invalid response.Please try again.\n");
-            System.out.print("Enter the base of your number (2, 8, 10 or 16): ");
-            choice = s.nextLine();
+            System.out.println("Do you want to do basic conversion or any conversion? (Only basic or any are viable inputs)");
+            input = s1.nextLine();
         }
-        int base = Integer.parseInt(choice);
+        if (input.equals("any")) {
+            System.out.print("Enter any base 10 number: ");
+            Scanner s = new Scanner(System.in);
+            String number = s.nextLine();
 
-        System.out.print("Enter your number: ");
-        String number = s.nextLine();
-
-        for (int i = 0; i < number.length(); i++) {
-            char c = number.charAt(i);
-            if (base == 2) {
-                if (c != '0' && c != '1') {
+            for (int i = 0; i < number.length(); i++) {
+                char c = number.charAt(i);
+                if(!((c >= '1' && c <= '9'))){
                     System.out.println("This is an invalid response. Please try again.");
-                    System.out.print("Enter your number: ");
-                    number = s.nextLine();
-                    i = -1;
-                }
-            } else if (base == 8) {
-                if (c < '0' || c > '7') {
-                    System.out.println("This is an invalid response. Please try again.");
-                    System.out.print("Enter your number: ");
-                    number = s.nextLine();
-                    i = -1;
-                }
-            } else if (base == 16) {
-                if (!((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F'))) {
-                    System.out.println("This is an invalid response. Please try again.");
-                    System.out.print("Enter your number: ");
+                    System.out.print("Enter any base 10 number: ");
                     number = s.nextLine();
                     i = -1;
                 }
             }
+            System.out.print("Enter a new base(1-64) to convert to: ");
+            int base = s.nextInt();
+
+            int count = 0;
+            while(count == 0) {
+                if ((base >= 1) && (base <= 64)) {
+                    count = 1;
+                } else {
+                    System.out.println("This is an invalid response. Please try again.");
+                    System.out.print("Enter a new base(1-64) to convert to: ");
+                    base = s.nextInt();
+                }
+            }
+            String n = number;
+            s.close();
+
+            NumberConverter nc = new NumberConverter(n, base);
+            System.out.print(nc.getConvertToAnyBase(base));
         }
 
-       // int n = Integer.parseInt(number);
-        String n = number;
-        s.close();
+        if (input.equals("basic")) {
+            System.out.print("Enter the base of your number (2, 8, 10, or 16): ");
 
-        NumberConverter nc = new NumberConverter(n, base);
-        int[] digits = nc.getDigits();
+            Scanner s = new Scanner(System.in);
+            String choice = s.nextLine();
+            while ((!choice.equals("2") && (!choice.equals("8")) && (!choice.equals("10")) && (!choice.equals("16")))) {
+                System.out.println("This is an invalid response.Please try again.\n");
+                System.out.print("Enter the base of your number (2, 8, 10 or 16): ");
+                choice = s.nextLine();
+            }
+            int base = Integer.parseInt(choice);
 
-        if(base == 2){
-            System.out.println("\nDecimal Number: " + Arrays.toString(nc.convertToDecimal()));
-            System.out.println("Octal Number: " + Arrays.toString(nc.convertToOctal()));
-            System.out.println("Hexadecimal Number: " + Arrays.toString(nc.convertToHexadecimal()));
-        }
-        if(base == 8){
-            System.out.println("\nDecimal Number: " + Arrays.toString(nc.convertToDecimal()));
-            System.out.println("Binary Number: " + Arrays.toString(nc.convertToBinary()));
-            System.out.println("Hexadecimal Number: " + Arrays.toString(nc.convertToHexadecimal()));
-        }
-        if(base == 10){
-            System.out.println("\nBinary Number: " + Arrays.toString(nc.convertToBinary()));
-            System.out.println("Octal Number: " + Arrays.toString(nc.convertToOctal()));
-            System.out.println("Hexadecimal Number: " + Arrays.toString(nc.convertToHexadecimal()));
-        }
-        if(base == 16){
-            System.out.println("\nDecimal Number: " + Arrays.toString(nc.convertToDecimal()));
-            System.out.println("Binary Number: " + Arrays.toString(nc.convertToBinary()));
-            System.out.println("Octal Number: " + Arrays.toString(nc.convertToOctal()));
+            System.out.print("Enter your number: ");
+            String number = s.nextLine();
 
+            for (int i = 0; i < number.length(); i++) {
+                char c = number.charAt(i);
+                if (base == 2) {
+                    if (c != '0' && c != '1') {
+                        System.out.println("This is an invalid response. Please try again.");
+                        System.out.print("Enter your number: ");
+                        number = s.nextLine();
+                        i = -1;
+                    }
+                } else if (base == 8) {
+                    if (c < '0' || c > '7') {
+                        System.out.println("This is an invalid response. Please try again.");
+                        System.out.print("Enter your number: ");
+                        number = s.nextLine();
+                        i = -1;
+                    }
+                } else if(base == 10){
+                    if(!((c >= '0' && c <= '9'))){
+                        System.out.println("This is an invalid response. Please try again.");
+                        System.out.print("Enter your number: ");
+                        number = s.nextLine();
+                        i = -1;
+                    }
+                }else if (base == 16) {
+                    if (!((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F'))) {
+                        System.out.println("This is an invalid response. Please try again.");
+                        System.out.print("Enter your number: ");
+                        number = s.nextLine();
+                        i = -1;
+                    }
+                }
+            }
+
+            // int n = Integer.parseInt(number);
+            String n = number;
+            s.close();
+
+            NumberConverter nc = new NumberConverter(n, base);
+            int[] digits = nc.getDigits();
+
+            if (base == 2) {
+                System.out.println(nc.getConvertToDecimal());
+                System.out.println(nc.getConvertToOctal());
+                System.out.println(nc.getConvertToHexaDecimal());
+            }
+            if (base == 8) {
+                System.out.println(nc.getConvertToDecimal());
+                System.out.println(nc.getConvertToBinary());
+                System.out.println(nc.getConvertToHexaDecimal());
+            }
+            if (base == 10) {
+                System.out.println(nc.getConvertToBinary());
+                System.out.println(nc.getConvertToOctal());
+                System.out.println(nc.getConvertToHexaDecimal());
+            }
+            if (base == 16) {
+                System.out.println(nc.getConvertToDecimal());
+                System.out.println(nc.getConvertToBinary());
+                System.out.println(nc.getConvertToOctal());
+
+            }
         }
     }
 }
-
--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -30,6 +30,52 @@ public class NumberConverter {
         return digits;
     }
 
+    public String getConvertToDecimal(){
+        int[] decimal1 = convertToDecimal();
+        String decimal = "\nDecimal Number: ";
+        for (int i = 0; i < decimal1.length; i++) {
+            decimal += decimal1[i];
+        }
+        return decimal;
+    }
+
+    public String getConvertToOctal(){
+        int[] octal1 = convertToOctal();
+        String octal = "Octal Number: ";
+        for (int i = 0; i < octal1.length; i++) {
+            octal += octal1[i];
+        }
+        return octal;
+    }
+
+    public String getConvertToBinary(){
+        int[] binary1 = convertToBinary();
+        String binary = "Binary Number: ";
+        for (int i = 0; i < binary1.length; i++) {
+            binary += binary1[i];
+        }
+        return binary;
+    }
+
+    public String getConvertToHexaDecimal(){
+        String[] hexa1 = convertToHexadecimal();
+        String hexa = "Hexadecimal Number: ";
+        for (int i = 0; i < hexa1.length; i++) {
+            hexa += hexa1[i];
+        }
+        return hexa;
+    }
+
+    public String getConvertToAnyBase(int base){
+        String[] anyBase1 = convertAnyBase(base);
+        String anyBase = "Base " + base + " Number: ";
+        for (int i = 0; i < anyBase1.length; i++) {
+            anyBase += anyBase1[i];
+        }
+        return anyBase;
+    }
+
+
     public int[] convertToDecimal() {
         int[] base10 = new int[1];
         if (base == 2) {
@@ -201,6 +247,193 @@ public class NumberConverter {
             if (arrayHex[i].equals("15")) {
                 arrayHex[i] = "F";
             }
+        }
+        return arrayHex;
+    }
+
+    public String[] convertAnyBase(int base){
+        int count = 0;
+        String originalNumberString = displayOriginalNumber().trim();
+        int base10 = Integer.parseInt(originalNumberString);
+        int base10V2 = base10;
+        while (base10V2 > 0) {
+            base10V2 = base10V2 / base;
+            count++;
+        }
+
+        String[] arrayHex = new String[count];
+        for(int i = 0; i < count; i++){
+            arrayHex[count-(i+1)] = "" + base10 % base;
+            base10 = base10 / base;
+        }
+
+        for (int i = 0; i < count; i++) {
+            if (arrayHex[i].equals("10")) {
+                arrayHex[i] = "A";
+            }
+            if (arrayHex[i].equals("11")) {
+                arrayHex[i] = "B";
+            }
+            if (arrayHex[i].equals("12")) {
+                arrayHex[i] = "C";
+            }
+            if (arrayHex[i].equals("13")) {
+                arrayHex[i] = "D";
+            }
+            if (arrayHex[i].equals("14")) {
+                arrayHex[i] = "E";
+            }
+            if (arrayHex[i].equals("15")) {
+                arrayHex[i] = "F";
+            }
+            if (arrayHex[i].equals("16")) {
+                arrayHex[i] = "G";
+            }
+            if (arrayHex[i].equals("17")) {
+                arrayHex[i] = "H";
+            }
+            if (arrayHex[i].equals("18")) {
+                arrayHex[i] = "I";
+            }
+            if (arrayHex[i].equals("19")) {
+                arrayHex[i] = "J";
+            }
+            if (arrayHex[i].equals("20")) {
+                arrayHex[i] = "K";
+            }
+            if (arrayHex[i].equals("21")) {
+                arrayHex[i] = "L";
+            }
+            if (arrayHex[i].equals("22")) {
+                arrayHex[i] = "M";
+            }
+            if (arrayHex[i].equals("23")) {
+                arrayHex[i] = "N";
+            }
+            if (arrayHex[i].equals("24")) {
+                arrayHex[i] = "O";
+            }
+            if (arrayHex[i].equals("25")) {
+                arrayHex[i] = "P";
+            }
+            if (arrayHex[i].equals("26")) {
+                arrayHex[i] = "Q";
+            }
+            if (arrayHex[i].equals("27")) {
+                arrayHex[i] = "R";
+            }
+            if (arrayHex[i].equals("28")) {
+                arrayHex[i] = "S";
+            }
+            if (arrayHex[i].equals("29")) {
+                arrayHex[i] = "T";
+            }
+            if (arrayHex[i].equals("30")) {
+                arrayHex[i] = "U";
+            }
+            if (arrayHex[i].equals("31")) {
+                arrayHex[i] = "V";
+            }
+            if (arrayHex[i].equals("32")) {
+                arrayHex[i] = "W";
+            }
+            if (arrayHex[i].equals("33")) {
+                arrayHex[i] = "X";
+            }
+            if (arrayHex[i].equals("34")) {
+                arrayHex[i] = "Y";
+            }
+            if (arrayHex[i].equals("35")) {
+                arrayHex[i] = "Z";
+            }
+            if (arrayHex[i].equals("36")) {
+                arrayHex[i] = "a";
+            }
+            if (arrayHex[i].equals("37")) {
+                arrayHex[i] = "b";
+            }
+            if (arrayHex[i].equals("38")) {
+                arrayHex[i] = "c";
+            }
+            if (arrayHex[i].equals("39")) {
+                arrayHex[i] = "d";
+            }
+            if (arrayHex[i].equals("40")) {
+                arrayHex[i] = "e";
+            }
+            if (arrayHex[i].equals("41")) {
+                arrayHex[i] = "f";
+            }
+            if (arrayHex[i].equals("42")) {
+                arrayHex[i] = "g";
+            }
+            if (arrayHex[i].equals("43")) {
+                arrayHex[i] = "h";
+            }
+            if (arrayHex[i].equals("44")) {
+                arrayHex[i] = "i";
+            }
+            if (arrayHex[i].equals("45")) {
+                arrayHex[i] = "j";
+            }
+            if (arrayHex[i].equals("46")) {
+                arrayHex[i] = "k";
+            }
+            if (arrayHex[i].equals("47")) {
+                arrayHex[i] = "l";
+            }
+            if (arrayHex[i].equals("48")) {
+                arrayHex[i] = "m";
+            }
+            if (arrayHex[i].equals("49")) {
+                arrayHex[i] = "n";
+            }
+            if (arrayHex[i].equals("50")) {
+                arrayHex[i] = "o";
+            }
+            if (arrayHex[i].equals("51")) {
+                arrayHex[i] = "p";
+            }
+            if (arrayHex[i].equals("52")) {
+                arrayHex[i] = "q";
+            }
+            if (arrayHex[i].equals("53")) {
+                arrayHex[i] = "r";
+            }
+            if (arrayHex[i].equals("54")) {
+                arrayHex[i] = "s";
+            }
+            if (arrayHex[i].equals("55")) {
+                arrayHex[i] = "t";
+            }
+            if (arrayHex[i].equals("56")) {
+                arrayHex[i] = "u";
+            }
+            if (arrayHex[i].equals("57")) {
+                arrayHex[i] = "v";
+            }
+            if (arrayHex[i].equals("58")) {
+                arrayHex[i] = "w";
+            }
+            if (arrayHex[i].equals("59")) {
+                arrayHex[i] = "x";
+            }
+            if (arrayHex[i].equals("60")) {
+                arrayHex[i] = "y";
+            }
+            if (arrayHex[i].equals("61")) {
+                arrayHex[i] = "z";
+            }
+            if (arrayHex[i].equals("62")) {
+                arrayHex[i] = "+";
+            }
+            if (arrayHex[i].equals("63")) {
+                arrayHex[i] = "/";
+            }
+            if (arrayHex[i].equals("64")) {
+                arrayHex[i] = "F";
+            }
+
         }
         return arrayHex;
     }
